be5c27b51282b461cc836ff0ae4d4dbfd977acae
Irzikri Naufaldi <89294307+zikardd@users.noreply.github.com>
2023-02-05 14:23:26 +0700

    Add files via upload

---
+++ 01 - Variable, Data type, and Syntax Writing/practice_1.ino
@@ -0,0 +1,26 @@
+//variabel, tipe data, penulisan syntax
+int led;
+boolean nilai = true; //dapat menggunakan true, false, 1, atau 0
+byte pin = 3; //byte dalam rentang nilai 0-255
+char karakter;
+int pin2 = -2; //int dalam rentang nilai -32.768 - 32.768
+unsigned int pin3= 5; // memiliki rentang nilai 0 sampai 65.535
+long pin4 = -10; // memiliki rentang -2miliar sampai +2mmliar
+unsigned long pin5 = 10; // memiliki rentang 0 sampai 4 miliar
+float pin6 = 5.0; // bilangan real
+double pin7 = 10.0; // bilangan real dengan kapasitas lebih banyak
+String text = "Hello world";
+
+
+/*Dua fungsi wajib arduino, setup dan loop*/
+//Pada setup, syntax hanya bekerja satu kali
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+//Pada loop, syntax bekerja secara berulang-ulang
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
